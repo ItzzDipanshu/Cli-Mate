c4ace11b0df9424eaa6b5b73e564ed84001b8388
ItzzDipanshu <dipanshu1231@gmail.com>
2024-08-21 01:42:46 +0530

    Added 5 day forecast

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const api = `http://api.openweathermap.org/geo/1.0/direct?q=`;
 const apiKey = `a245db8684dc638c7145426200da99c2`;
 
 const searchBtn = document.querySelector("#searchBtn");
 
 const fetchCitySuggestions = async () => {
+  const api = `http://api.openweathermap.org/geo/1.0/direct?q=`;
   const input = document.querySelector("#search-input").value.trim();
   const suggestionList = document.querySelector(".citySuggestions");
 
@@ -54,7 +54,7 @@ const checkWeather = async (city) => {
 
   const data = await response.json();
 
-  console.log(data);
+//   console.log(data);
 
   cityName.textContent = `${data.name}, ${data.sys.country}`;
 
@@ -91,8 +91,6 @@ const checkWeather = async (city) => {
 
   sunrise.textContent = `${hoursSR}:${minutesSR}`;
 
-
-
   const sunsetTimestamp = data.sys.sunset;
   const sunsetTime = new Date(sunsetTimestamp * 1000);
 
@@ -100,7 +98,84 @@ const checkWeather = async (city) => {
   const minutesSS = sunsetTime.getMinutes().toString().padStart(2, "0");
 
   sunset.textContent = `${hoursSS}:${minutesSS}`;
+
+  const lat = data.coord.lat;
+  const lon =  data.coord.lon;
+
+  fourDaysForecast(lat, lon);
+
+
+
 };
+
+const fourDaysForecast = async(lat, lon)=>{
+    const apiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+
+    const btns = document.querySelectorAll(".btn");
+
+    const response= await fetch(apiURL);
+    const data = await response.json();
+    // console.log(data);
+
+
+    const fiveDays = [];
+    const fiveDaysForecast = data.list.filter(forecast=>{
+        const forecastDate = new Date(forecast.dt_txt).getDate();
+
+        if(!fiveDays.includes(forecastDate)){
+            return fiveDays.push(forecastDate);
+        }
+    });
+
+    fiveDaysForecast.shift();
+    // console.log(fiveDaysForecast);
+
+    btns.forEach((btn, index)=>{
+        btn.textContent = (fiveDaysForecast[index].dt_txt).split(' ')[0];
+
+        btn.addEventListener("click",()=>{
+            forecastInfo(fiveDaysForecast[index]);
+        })
+    })
+
+}
+
+const forecastInfo = (obj) => {
+    // console.log(obj);
+    const weatherIconFDF = document.querySelector("#weatherIconFDF");
+    const descFDF = document.querySelector("#descFDF");
+    const tmax = document.querySelector("#tmax");
+    const tmin = document.querySelector("#tmin");
+    const windSpeed = document.querySelector("#wspeed");
+    const atmoPressure = document.querySelector("#atmoPressureFDF");
+    const humidity = document.querySelector("#humidityFDF");
+    const visibilityFDF = document.querySelector("#visibilityFDF");
+
+    if (obj.weather[0].main == "Clouds") {
+        weatherIconFDF.src = "./images/clouds.png";
+      } else if (obj.weather[0].main == "Clear") {
+        weatherIconFDF.src = "./images/clear.png";
+      } else if (obj.weather[0].main == "Rain") {
+        weatherIconFDF.src = "./images/rain.png";
+      } else if (obj.weather[0].main == "Drizzle") {
+        weatherIconFDF.src = "./images/drizzle.png";
+      } else if (obj.weather[0].main == "Mist") {
+        weatherIconFDF.src = "./images/mist.png";
+      } else if (obj.weather[0].main == "Snow") {
+        weatherIconFDF.src = "./images/snow.png";
+      } else if (obj.weather[0].main == "Fog") {
+        weatherIconFDF.src = "./images/fog.png";
+      }
+
+      descFDF.textContent = obj.weather[0].description;
+      tmax.textContent = (obj.main.temp_max - 273.15).toFixed(1);
+      tmin.textContent = (obj.main.temp_min - 273.15).toFixed(1);
+      windSpeed.textContent = obj.wind.speed;
+      atmoPressure.textContent = obj.main.pressure;
+      humidity.textContent = obj.main.humidity;
+      visibilityFDF.textContent = obj.visibility / 1000;
+}
 
 const btnClick = () => {
   const searchInput = document.querySelector("#search-input");
